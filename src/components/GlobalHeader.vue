f57a87302653b7a0fd113e7b1efbc65befc6140d
Eva813 <as45986@gmail.com>
2021-11-13 16:42:00 +0800

    add header component

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <!-- navbar-brand -->
+    <a class="navbar-brand" href="#">談天說地</a>
+    <!-- 判斷沒有登入，要顯示的樣子 -->
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">登入</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">注册</a>
+      </li>
+    </ul>
+    <!-- 若登入的話 -->
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">你好 {{ user.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+//組件開立的第一步，建立它的 interface
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+export default defineComponent({
+  name: "GlobalHeader",
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style>
+</style>
