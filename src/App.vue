be5bcdbc2b6a146531492bfb2fd39e09131a430c
Eva813 <as45986@gmail.com>
2021-11-21 14:31:50 +0800

    驗證表單1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser"></GlobalHeader>
-    <column-list :list="outlist"></column-list>
+    <!-- <column-list :list="outlist"></column-list> -->
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">email</label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div class="form-text" v-if="emailRef.error">
+          {{ emailRef.message }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密碼</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 //將其中的interface也引入
 import ColumnList, { ColumnProps } from "@/components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "@/components/GlobalHeader.vue";
-
+const emailReg =
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const currentUser: UserProps = {
   isLogin: true,
   name: "Eva",
@@ -57,13 +81,29 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: "App",
   components: {
-    ColumnList,
+    // ColumnList,
     GlobalHeader,
   },
   setup() {
+    const emailRef = reactive({
+      val: "",
+      error: false,
+      message: "",
+    });
+    const validateEmail = () => {
+      if (emailRef.val.trim() === "") {
+        emailRef.error = true;
+        emailRef.message = "不能為空白";
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true;
+        emailRef.message = "email 不符合格式";
+      }
+    };
     return {
       outlist: testData,
       currentUser,
+      emailRef,
+      validateEmail,
     };
   },
 });
