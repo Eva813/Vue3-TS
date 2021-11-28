2e5f1134f7e6d72deb57a928cf02363022e9f168
Eva813 <as45986@gmail.com>
2021-11-28 10:51:15 +0800

    modify

---
+++ src/components/ValidateForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="validate-form-container">
+    <slot name="default"></slot>
+    <div class="submit-area" @click.prevent="submitForm">
+      <slot name="submit">
+        <button type="submit" class="btn btn-primary">提交</button>
+      </slot>
+    </div>
+  </form>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  emits: ["form-submit"],
+  setup(props, context) {
+    const submitForm = () => {
+      //第二個是拿到最終的驗證並觸發事件
+      context.emit("form-submit", true);
+    };
+    return {
+      submitForm,
+    };
+  },
+});
+</script>
---
+++ src/components/test.vue
@@ -0,0 +1,8 @@
+<template>加上第一行</template>
+d
+<script>
+export default {};
+</script>
+
+<style>
+</style>
